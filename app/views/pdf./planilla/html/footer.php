a41c52bd084bbd26dfe4fd80c826fce4f6ab5353
Andres Alejo <Aalejog09@gmail-com>
2024-10-13 11:28:31 -0400

    2do commit

--- app/views/pdf./planilla/html/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <script src="<?php echo APP;?>app/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="<?php echo APP;?>app/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-</footer>
-
-</body>
-</html>
